ed3dac05abe7c3525066bb569e3bef275c5cca1c
eshjay <olushola.abiodun@ovi123>
2020-08-20 02:18:24 +0100

    New project updated

--- js/myscript.js
+++ js/myscript.js
@@ -121,4 +121,17 @@ window.onload = function() {
 			}
 
 		} , { offset: '85%' } );
-	};
+    };
+     
+    // Modal box-container
+
+    const modalBtn = document.querySelector(".modal-btn");
+    const modal = document.querySelector(".modal-overlay");
+    const closeBtn = document.querySelector(".close-btn");
+    
+    modalBtn.addEventListener("click", function () {
+      modal.classList.add("open-modal");
+    });
+    closeBtn.addEventListener("click", function () {
+      modal.classList.remove("open-modal");
+    });
